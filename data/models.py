cfef5c72f8d2c5c42097996c52a9dcebf1fa3b4d
Mahesh <91116963+MaheshKathir@users.noreply.github.com>
2024-02-19 18:43:17 +0530

    delete

--- data/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class list(models.Model):
-
-    cars = models.CharField(max_length=150, default='')
-    News_Paper = models.CharField(max_length=150, default='')
